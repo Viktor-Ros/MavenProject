97c389cf603c9c99ed6f5980c1d4cdb3b88ae9a5
Viktor <Viktor@DESKTOP-7C8D4ED>
2020-07-29 17:47:03 +0300

    Подключил плагин

--- HWAnton2/src/test/java/tests/CucumberTest.java
+++ HWAnton2/src/test/java/tests/CucumberTest.java
@@ -1,13 +1,14 @@
 package tests;
 
+import org.junit.jupiter.api.condition.EnabledForJreRange;
 import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
-
+@EnabledForJreRange
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"pretty"},
+        plugin = {"io.qameta.allure.cucumber5jvm.AllureCucumber5Jvm"},
         glue = {"framework.stepdef"},
         features = {"src/test/resources/cucumbers"},
         tags = {"@checkEmailErrorMessage"}
