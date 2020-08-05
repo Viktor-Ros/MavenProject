6fcbd8ec2e6c1351acff25b3281d42b60ded1d6b
Viktor <Viktor@DESKTOP-7C8D4ED>
2020-08-05 17:34:53 +0300

    Класс для утилит аллюра

--- HWArcadiy3/src/main/java/framework/utils/AllureUtils.java
+++ HWArcadiy3/src/main/java/framework/utils/AllureUtils.java
@@ -1,30 +1,14 @@
 package framework.utils;
 
-import static io.qameta.allure.junitplatform.AllureJunitPlatform.ALLURE_FIXTURE;
-import static io.qameta.allure.junitplatform.AllureJunitPlatform.EVENT_FAILURE;
-import static io.qameta.allure.junitplatform.AllureJunitPlatform.TEAR_DOWN;
-
-import java.lang.reflect.Method;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
-
-import org.junit.jupiter.api.extension.ExtensionContext;
-import org.junit.jupiter.api.extension.ReflectiveInvocationContext;
-import org.junit.jupiter.api.extension.InvocationInterceptor.Invocation;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
 import framework.managers.DriverManager;
 import io.qameta.allure.Attachment;
 import io.qameta.allure.junit5.AllureJunit5;
-import io.qameta.allure.model.Status;
-import io.qameta.allure.model.StatusDetails;
-import io.qameta.allure.util.ResultsUtils;
 
-public class AllureUtils extends AllureJunit5{
+public class AllureUtils{
 	
-
     @Attachment(value = "screenshot", type = "image/png")
     public static byte[] addScreenshot() {
         return ((TakesScreenshot) DriverManager.getDriver()).getScreenshotAs(OutputType.BYTES);
